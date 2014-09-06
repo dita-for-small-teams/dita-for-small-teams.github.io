09cc5f99d0f7c3046a3159f5873f838b7fc0a9c9
Eliot Kimber <ekimber@contrext.com>
2014-09-06 10:57:18 -0500

    Updated pointer to project location

--- toc.js
+++ toc.js
@@ -9,33 +9,33 @@
       var root = tree.getRoot();
 
       
-var objd64e75 = {
+var objd64e89 = {
   label: " DITA For Small Teams",
   href: "topics/dita-for-small-teams.html", 
   target:"contentwin"
 };
-var d64e75 = new YAHOO.widget.TextNode(objd64e75, root, false);
+var d64e89 = new YAHOO.widget.TextNode(objd64e89, root, false);
 
-var objd64e100 = {
+var objd64e114 = {
   label: " DITA for Small Teams Tools",
   href: "topics/dfst-tools.html", 
   target:"contentwin"
 };
-var d64e100 = new YAHOO.widget.TextNode(objd64e100, d64e75, false);
+var d64e114 = new YAHOO.widget.TextNode(objd64e114, d64e89, false);
 
-var objd64e125 = {
+var objd64e139 = {
   label: " git: Distributed and Shared Access to Content",
   href: "topics/tools-git.html", 
   target:"contentwin"
 };
-var d64e125 = new YAHOO.widget.TextNode(objd64e125, d64e100, false);
+var d64e139 = new YAHOO.widget.TextNode(objd64e139, d64e114, false);
 
-var objd64e150 = {
+var objd64e164 = {
   label: " eXist-db: Enabling Search and Link Management",
   href: "topics/tools-exist-db.html", 
   target:"contentwin"
 };
-var d64e150 = new YAHOO.widget.TextNode(objd64e150, d64e100, false);
+var d64e164 = new YAHOO.widget.TextNode(objd64e164, d64e114, false);
 
       
       tree.draw(); 
